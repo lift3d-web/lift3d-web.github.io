470251f8ad8661e6742d506b9bd57397206202e4
buttomnutstoast <mackintoshtoast@gmail.com>
2024-02-12 21:33:16 -0500

    Update real-world videos

--- static/videos/realworld/process.sh
+++ static/videos/realworld/process.sh
@@ -3,9 +3,12 @@ for d in ./* ; do
     echo "$d"
     task=${d%????}
     task="${task:2}"
-    task_str=$(sed 's/_/ /g' <<< "$task")
     new_d=new_$task.mp4
+
+    task_str=${task%??}
+    task_str=$(sed 's/_/ /g' <<< "$task_str")
     echo $new_d $task_str
+
     ffmpeg -i $d -vf "drawtext=text=$task_str:fontfile=/Library/Fonts/Arial\ Unicode.ttf:fontsize=65:fontcolor=white:x=12:y=12" -codec:a copy ../out.mp4
     ffmpeg -i ../out.mp4 -vf "scale=432:360" $new_d
     rm ../out.mp4
